d5cadc064bd2d9f8f01b3ade667b69bc21b7754f
Nurman <khozhamuratov.nn@gmail.com>
2024-01-21 21:37:19 +0500

    completed

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,7 +1,10 @@
+import { redirect } from 'next/navigation'
+
 export default function HomeLayout({
 	children,
 }: {
 	children: React.ReactNode
 }) {
+	redirect('/login')
 	return <div>{children}</div>
 }
